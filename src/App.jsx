38aaaf861ff523f34f6f9c81dfa73405c128b8c6
Victor Prete <vprete@tuta.io>
2022-09-13 21:14:09 -0400

    feat: add meme and header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import React from "react"
+import Header from "../components/Header"
+import Meme from "../components/Meme"
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <p>app</p>
-    </div>
+      <div>
+          <Header />
+          <Meme />
+      </div>
   )
 }
 
